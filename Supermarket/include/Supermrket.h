d80f223b56559ce57fc016568391847d845f901e
Hossam Ragab <hossm.alasklany@gmail.com>
2024-01-12 14:59:04 +0200

    main class

--- Supermarket/include/Supermrket.h
+++ Supermarket/include/Supermrket.h
@@ -1,17 +1,21 @@
 #ifndef SUPERMRKET_H
 #define SUPERMRKET_H
 
-
 //main class that integrates all components
 #include "UserInterface.h"
+#include "Admin.h"
+#include "Buyer.h"
+#include "Inventory.h"
 
 class MainClass
 {
 private:
+    Inventory inventory;
+    Admin admin;
+    Buyer buyer;
     UserInterface ui;
 public:
-    MainClass();
-    MainClass(UserInterface& ui);
+    MainClass(string inventoryFileName);
     void run();
 
     ~MainClass();
