cd05bc54fd3be255b3f9999ef3d40831b3faff41
Hossam Ragab <hossm.alasklany@gmail.com>
2024-01-12 14:58:47 +0200

    implement buyer class

--- Supermarket/include/Buyer.h
+++ Supermarket/include/Buyer.h
@@ -8,9 +8,13 @@ class Buyer
 {
 private:
     Inventory& inventory;
+    float balance; //buyer balance
+
 public:
     Buyer(Inventory& inventory);
+    void viewProducts();
     void purchaseProduct();
+    void addBalance(float amount);
 
     ~Buyer();
 };
