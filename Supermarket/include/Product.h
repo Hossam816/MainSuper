46723f150d2e895a917659c747702e1163df1b26
Hossam Ragab <hossm.alasklany@gmail.com>
2024-01-12 14:58:58 +0200

    all product mehtod done

--- Supermarket/include/Product.h
+++ Supermarket/include/Product.h
@@ -1,7 +1,6 @@
 #ifndef PRODUCT_H
 #define PRODUCT_H
 #include <string>
-
 //applying Encapsulation concept
 
 using namespace std;
@@ -12,22 +11,27 @@ class Product
        int code;
        float price;
        float discount;
+       int quantity;
        string name;
 
    public:
-    Product(int code,string name,float price,float discount);
+    Product(int code, const string& name, float price, float discount);
+    Product(int code, const string& name, float price, float discount, int quantity);
+
+
     //Getters
     int getCode();
     string getName();
     float getPrice();
     float getDiscount();
+    int getQuantity();
 
     //Setters
     void setCode(int code);
     void setName(string name);
     void setPrice(float price);
     void setDiscount(float discount);
-
+    void setQuantity(int quantity);
     void displayProduct();
 
     ~Product();
